0f0a2ad90cf2e2f7fef6fad8eee0517c77f6373d
Aleksey <kladkan@yandex.ru>
2018-10-12 11:40:47 +0300

    Update animals.php

--- animals.php
+++ animals.php
@@ -8,11 +8,17 @@
     'Antarctica' => ['Mirounga', 'Lobodon carcinophagus', 'Aptenodytes forsteri']
   ];
 
+  echo '<p>1. Исходный массив:</p>';
   var_dump($all_animals);
 
+  echo '<p>Количество всех животных = ' . (count($all_animals, COUNT_RECURSIVE) - count($all_animals)) . '<p>';
+
+  var_dump(explode(' ', $all_animals['Africa']));
+
+  echo '<p>2. Названия, состоящие из двух слов:</p>';
 
 
-
+  $animals_2_word = [];
 
 
 
