2870ab381c4323b41807ba3574d3707c146b7edf
Aleksey <kladkan@yandex.ru>
2018-10-12 10:37:56 +0300

    Update animals.php

--- animals.php
+++ animals.php
@@ -1,12 +1,12 @@
 <?php
   ini_set('display_errors', 1);
   ini_set('errors_reporting', E_ALL);
-  $all_animals = array(
-    'Africa' => array('Mammuthus columbi', 'Ciconia', 'Panthera pardus', 'Crocodylus niloticus'),
-    'Australia' => array('Macropus rufus', 'Phascolarctos cinereus', 'Struthionidae'),
-    'Eurasia' => array('Vulpes lagopus', 'Canis lupus', 'Lynx', 'Odobenus rosmarus'),
-    'Antarctica' => array('Mirounga', 'Lobodon carcinophagus', 'Aptenodytes forsteri')
-  );
+  $all_animals = [
+    'Africa' => ['Mammuthus columbi', 'Ciconia', 'Panthera pardus', 'Crocodylus niloticus'],
+    'Australia' => ['Macropus rufus', 'Phascolarctos cinereus', 'Struthionidae'],
+    'Eurasia' => ['Vulpes lagopus', 'Canis lupus', 'Lynx', 'Odobenus rosmarus'],
+    'Antarctica' => ['Mirounga', 'Lobodon carcinophagus', 'Aptenodytes forsteri']
+  ];
 
   var_dump($all_animals);
 
